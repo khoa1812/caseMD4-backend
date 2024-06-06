4d59d660267283ed949b668d04b6d041064146c3
khoa1812 <khoatrinh2909@gmail.com>
2024-06-06 09:22:49 +0700

    start

---
+++ src/main/java/com/example/casestudymd4/CasestudyMd4Application.java
@@ -0,0 +1,13 @@
+package com.example.casestudymd4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudyMd4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasestudyMd4Application.class, args);
+    }
+
+}
